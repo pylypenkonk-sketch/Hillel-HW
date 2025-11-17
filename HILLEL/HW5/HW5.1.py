ff3414765fb6784dabb679810c6bf6b223916fa7
pylypenkonk <pylypenkonk@gmail.com>
2025-11-17 20:52:39 +0200

    Commit

--- HILLEL/HW5/HW5.1.py
+++ HILLEL/HW5/HW5.1.py
@@ -3,17 +3,29 @@ import string
 
 var_name = input("Змінна: ")
 
-punct = string.punctuation.replace("_", "")
+if not var_name:
+    print(False)
 
-if var_name in keyword.kwlist:
+elif keyword.iskeyword(var_name):
     print(False)
-elif var_name and var_name[0].isdigit():
+
+elif var_name[0].isdigit():
     print(False)
-elif not all(ch.islower() or ch == "_" or ch.isdigit() for ch in var_name):
+
+elif any(ch.isupper() for ch in var_name):
     print(False)
-elif any(ch in punct for ch in var_name):
+
+elif " " in var_name:
     print(False)
-elif var_name.count("_") > 1:
+
+elif any(ch in string.punctuation.replace("_", "") for ch in var_name):
     print(False)
+
+elif all(ch == "_" for ch in var_name) and len(var_name) > 1:
+    print(False)
+
+elif not all(ch.islower() or ch.isdigit() or ch == "_" for ch in var_name):
+    print(False)
+
 else:
     print(True)
