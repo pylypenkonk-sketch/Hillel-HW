d460754ac4eec64464b6daf36f9b6abcae66ea95
pylypenkonk <pylypenkonk@gmail.com>
2025-12-23 10:42:37 +0200

    Commit

---
+++ HILLEL/HW9/hw9.2.py
@@ -0,0 +1,13 @@
+def difference(*args):
+    if not args:
+        return 0
+    return round(max(args) - min(args), 2)
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+
+print('OK')
+print(difference(3, 7, 2, 8.5))
+print(difference())
