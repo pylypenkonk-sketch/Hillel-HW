204f0df9a168049dcb6b6cda4064805066b08828
pylypenkonk <pylypenkonk@gmail.com>
2025-12-23 10:27:17 +0200

    Commit

--- HILLEL/HW7/hw7.1.py
+++ HILLEL/HW7/hw7.1.py
@@ -1,7 +1,10 @@
 def say_hi(name, age):
     return f"Hi. My name is {name} and I'm {age} years old"
 
-name = input("Введіть своє ім'я: ")
-age = int(input("Введіть свій вік: "))
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 
-print(say_hi(name, age))
+print('ОК')
+print(say_hi("Maria", 25))
+print(say_hi("John", 40))
+
